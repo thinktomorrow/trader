af15fa92ce7e815b72dbe8125b7bb78d4db5daaa
Ben Cavens <cavensben@gmail.com>
2018-02-13 12:42:41 +0100

    Add saleId placeholder

--- src/Sales/Domain/SaleId.php
+++ src/Sales/Domain/SaleId.php
@@ -7,4 +7,13 @@ use Thinktomorrow\Trader\Common\Domain\AggregateId;
 class SaleId
 {
     use AggregateId;
+
+    /**
+     * Allow to create placeholder itemId. Item is assigned an id after being stored
+     * Before this, a placeholder is required for the domain
+     */
+    public static function placeholder()
+    {
+        return new static('xxx');
+    }
 }
