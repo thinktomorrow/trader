5db5fee939482f855b3df879864a54396bbe598c
Ben Cavens <cavensben@gmail.com>
2022-09-06 13:39:24 +0200

    you copypaster

--- src/Domain/Model/Country/CountryRepository.php
+++ src/Domain/Model/Country/CountryRepository.php
@@ -5,9 +5,9 @@ namespace Thinktomorrow\Trader\Domain\Model\Country;
 
 interface CountryRepository
 {
-    public function save(Country $customer): void;
+    public function save(Country $country): void;
 
-    public function find(CountryId $customerId): Country;
+    public function find(CountryId $countryId): Country;
 
-    public function delete(CountryId $customerId): void;
+    public function delete(CountryId $countryId): void;
 }
