4854b10ea97271a10b96bf0de6b6e692833fa20c
BenCavens <BenCavens@users.noreply.github.com>
2025-10-30 09:51:51 +0000

    Fix styling

--- src/Infrastructure/Shop/CustomerAuth/Controllers/CustomerRegisterController.php
+++ src/Infrastructure/Shop/CustomerAuth/Controllers/CustomerRegisterController.php
@@ -7,13 +7,13 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use function redirect;
+use function route;
 use Thinktomorrow\Trader\Application\Customer\CustomerApplication;
 use Thinktomorrow\Trader\Application\Customer\RegisterCustomer;
 use Thinktomorrow\Trader\Domain\Model\Customer\CustomerRepository;
 use Thinktomorrow\Trader\Domain\Model\Customer\Events\CustomerHasLoggedIn;
 use Thinktomorrow\Trader\Infrastructure\Shop\CustomerAuth\CustomerModel;
-use function redirect;
-use function route;
 
 class CustomerRegisterController extends Controller
 {
@@ -22,8 +22,7 @@ class CustomerRegisterController extends Controller
     public function __construct(
         private CustomerApplication $customerApplication,
         private CustomerRepository  $customerRepository,
-    )
-    {
+    ) {
         $this->middleware('customer-guest');
     }
 
@@ -46,18 +45,19 @@ class CustomerRegisterController extends Controller
             'lastname' => ['required', 'string', 'max:200'],
             'email' => ['required', 'string', 'email', 'max:200', 'unique:trader_customers,email'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'company' => ['required_if:is_business,true', 'string', 'max:200']
+            'company' => ['required_if:is_business,true', 'string', 'max:200'],
         ]);
 
         $customerId = $this->customerApplication->registerCustomer(new RegisterCustomer(
             $request->email,
-            !!$request->is_business,
-            app()->getLocale(), [
+            ! ! $request->is_business,
+            app()->getLocale(),
+            [
                 'firstname' => $request->firstname,
                 'lastname' => $request->lastname,
                 'company' => $request->company ?? null,
                 'vat_number' => $request->vat_number ?? null,
-                'phone' => $request->phone ?? null
+                'phone' => $request->phone ?? null,
             ]
         ));
 
