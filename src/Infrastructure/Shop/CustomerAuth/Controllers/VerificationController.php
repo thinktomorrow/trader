f6aea7966568a69bf4ca449808692fbf6f795c8a
Ben Cavens <cavensben@gmail.com>
2025-11-13 11:30:25 +0100

    Fix

--- src/Infrastructure/Shop/CustomerAuth/Controllers/VerificationController.php
+++ src/Infrastructure/Shop/CustomerAuth/Controllers/VerificationController.php
@@ -26,8 +26,7 @@ class VerificationController extends Controller
 
     public function __construct()
     {
-        $this->middleware('customer-auth')->only('show', 'resend');
-        ;
+        $this->middleware('customer-auth')->only('show', 'resend');;
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
@@ -40,7 +39,7 @@ class VerificationController extends Controller
 
     public function show(Request $request)
     {
-        return $request->user()->hasVerifiedEmail()
+        return Auth::guard('customer')->user()->hasVerifiedEmail()
             ? redirect($this->redirectPath())
             : view('trader::customer.auth.verify');
     }
@@ -49,7 +48,7 @@ class VerificationController extends Controller
     {
         $customer = CustomerModel::findOrFail($request->route('id'));
 
-        if (! hash_equals((string)$request->route('hash'), sha1($customer->getEmailForVerification()))) {
+        if (!hash_equals((string)$request->route('hash'), sha1($customer->getEmailForVerification()))) {
             throw new AuthorizationException();
         }
         if ($customer->hasVerifiedEmail()) {
