7633de04469db75eedcf48bc4e79e9f4fd4ed5bb
Ben Cavens <cavensben@gmail.com>
2026-02-25 13:59:18 +0100

    Refactor TaxonomyRepositoryTest to use CatalogContext drivers

--- tests/Infrastructure/Repositories/TaxonomyRepositoryTest.php
+++ tests/Infrastructure/Repositories/TaxonomyRepositoryTest.php
@@ -3,117 +3,74 @@ declare(strict_types=1);
 
 namespace Tests\Infrastructure\Repositories;
 
-use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\Infrastructure\TestCase;
-use Thinktomorrow\Trader\Domain\Model\Taxon\Taxon;
-use Thinktomorrow\Trader\Domain\Model\Taxon\TaxonId;
 use Thinktomorrow\Trader\Domain\Model\Taxonomy\Exceptions\CouldNotFindTaxonomy;
-use Thinktomorrow\Trader\Domain\Model\Taxonomy\Taxonomy;
 use Thinktomorrow\Trader\Domain\Model\Taxonomy\TaxonomyId;
-use Thinktomorrow\Trader\Domain\Model\Taxonomy\TaxonomyState;
-use Thinktomorrow\Trader\Domain\Model\Taxonomy\TaxonomyType;
-use Thinktomorrow\Trader\Infrastructure\Laravel\Repositories\MysqlTaxonomyRepository;
-use Thinktomorrow\Trader\Infrastructure\Laravel\Repositories\MysqlTaxonRepository;
-use Thinktomorrow\Trader\Infrastructure\Test\Repositories\InMemoryTaxonomyRepository;
-use Thinktomorrow\Trader\Infrastructure\Test\Repositories\InMemoryTaxonRepository;
+use Thinktomorrow\Trader\Testing\Catalog\CatalogContext;
 
 final class TaxonomyRepositoryTest extends TestCase
 {
-    #[DataProvider('taxonomies')]
-    public function test_it_can_save_and_find_a_taxonomy(Taxonomy $taxonomy)
+    public function test_it_can_save_and_find_a_taxonomy()
     {
-        foreach ($this->taxonomyRepositories() as $taxonomyRepository) {
-            $taxonomyRepository->save($taxonomy);
-            $taxonomy->releaseEvents();
+        foreach (CatalogContext::drivers() as $catalog) {
 
-            $this->assertEquals($taxonomy, $taxonomyRepository->find($taxonomy->taxonomyId));
+            $repository = $catalog->repos()->taxonomyRepository();
+
+            $catalog->dontPersist();
+
+            $taxonomy = $catalog->createTaxonomy();
+
+            $repository->save($taxonomy);
+
+            $this->assertEquals($taxonomy, $repository->find($taxonomy->taxonomyId));
         }
     }
 
     public function test_it_can_find_many_taxonomies_by_taxon_id()
     {
-        $taxonomy = Taxonomy::create(
-            TaxonomyId::fromString('xxx'),
-            TaxonomyType::property,
-        );
+        /** @var CatalogContext $catalog */
+        foreach (CatalogContext::drivers() as $catalog) {
 
-        $taxon = Taxon::create(
-            TaxonId::fromString('yyy'),
-            $taxonomy->taxonomyId,
-        );
+            $repository = $catalog->repos()->taxonomyRepository();
 
-        foreach ($this->taxonomyRepositories() as $i => $taxonomyRepository) {
-            $taxonomyRepository->save($taxonomy);
-            $taxonomy->releaseEvents();
+            $taxonomy = $catalog->createTaxonomy();
+            $taxon = $catalog->createTaxon('taxon-aaa', $taxonomy->taxonomyId->get());
 
-            $this->taxonRepositories()[$i]->save($taxon);
-            $taxon->releaseEvents();
-
-            $this->assertEquals([$taxonomy], $taxonomyRepository->findManyByTaxa([$taxon->taxonId->get()]));
+            $this->assertEquals([$taxonomy], $repository->findManyByTaxa([$taxon->taxonId->get()]));
         }
     }
 
-    #[DataProvider('taxonomies')]
-    public function test_it_can_delete_a_taxonomy(Taxonomy $taxonomy)
+    public function test_it_can_delete_a_taxonomy()
     {
         $taxonomiesNotFound = 0;
 
-        foreach ($this->taxonomyRepositories() as $taxonomyRepository) {
-            $taxonomyRepository->save($taxonomy);
-            $taxonomyRepository->delete($taxonomy->taxonomyId);
+        /** @var CatalogContext $catalog */
+        foreach (CatalogContext::drivers() as $catalog) {
+
+            $repository = $catalog->repos()->taxonomyRepository();
+
+            $taxonomy = $catalog->createTaxonomy();
+
+            $repository->delete($taxonomy->taxonomyId);
 
             try {
-                $taxonomyRepository->find($taxonomy->taxonomyId);
+                $repository->find($taxonomy->taxonomyId);
             } catch (CouldNotFindTaxonomy $e) {
                 $taxonomiesNotFound++;
             }
         }
 
-        $this->assertEquals(count(iterator_to_array($this->taxonomyRepositories())), $taxonomiesNotFound);
+        $this->assertCount($taxonomiesNotFound, CatalogContext::drivers());
     }
 
     public function test_it_can_generate_a_next_reference()
     {
-        foreach ($this->taxonomyRepositories() as $taxonomyRepository) {
-            $this->assertInstanceOf(TaxonomyId::class, $taxonomyRepository->nextReference());
+        /** @var CatalogContext $catalog */
+        foreach (CatalogContext::drivers() as $catalog) {
+
+            $repository = $catalog->repos()->taxonomyRepository();
+
+            $this->assertInstanceOf(TaxonomyId::class, $repository->nextReference());
         }
-    }
-
-    private static function taxonomyRepositories(): \Generator
-    {
-        yield new InMemoryTaxonomyRepository();
-        yield new MysqlTaxonomyRepository();
-    }
-
-    private function taxonRepositories(): array
-    {
-        return [
-            new InMemoryTaxonRepository(),
-            new MysqlTaxonRepository(),
-        ];
-    }
-
-    public static function taxonomies(): \Generator
-    {
-        $taxonomy = Taxonomy::create(
-            TaxonomyId::fromString('xxx'),
-            TaxonomyType::property,
-        );
-
-        $taxonomy->addData(['foo' => 'bar']);
-
-        yield [$taxonomy];
-
-        $taxonomy = Taxonomy::create(
-            TaxonomyId::fromString('xxx'),
-            TaxonomyType::property,
-        );
-
-        $taxonomy->addData(['foo' => 'bar']);
-
-        $taxonomy->changeState(TaxonomyState::queued_for_deletion);
-        $taxonomy->changeOrder(555);
-
-        yield [$taxonomy];
     }
 }
