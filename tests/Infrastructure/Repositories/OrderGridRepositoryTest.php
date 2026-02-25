33df2ecc119a4859fadd9f47a147590890f7e1c3
Ben Cavens <cavensben@gmail.com>
2026-02-25 10:43:30 +0100

    Refactor OrderGridRepositoryTest to use OrderContext drivers and symlink trader-testing package

--- tests/Infrastructure/Repositories/OrderGridRepositoryTest.php
+++ tests/Infrastructure/Repositories/OrderGridRepositoryTest.php
@@ -6,13 +6,7 @@ namespace Tests\Infrastructure\Repositories;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Tests\Infrastructure\TestCase;
-use Thinktomorrow\Trader\Application\Order\Grid\OrderGridItem;
 use Thinktomorrow\Trader\Domain\Model\Order\State\DefaultOrderState;
-use Thinktomorrow\Trader\Infrastructure\Laravel\Models\DefaultOrderGridItem;
-use Thinktomorrow\Trader\Infrastructure\Laravel\Repositories\MysqlOrderGridRepository;
-use Thinktomorrow\Trader\Infrastructure\Laravel\Repositories\MysqlOrderRepository;
-use Thinktomorrow\Trader\Infrastructure\Test\TestContainer;
-use Thinktomorrow\Trader\Infrastructure\Test\TestTraderConfig;
 
 class OrderGridRepositoryTest extends TestCase
 {
@@ -22,77 +16,103 @@ class OrderGridRepositoryTest extends TestCase
     {
         parent::setUp();
 
-        $this->prepareWorldForOrder(1);
+        // Order grid repository is only in mysql available, not in memory.
         $this->order = $this->orderContext->createDefaultOrder();
-        (new TestContainer())->get(MysqlOrderRepository::class)->save($this->order);
     }
 
     public function test_it_fetches_grid_orders()
     {
-        $gridItems = $this->getMysqlGridRepository()->getResults();
+        $results = $this->orderContext->repos()->orderGridRepository()->getResults();
 
-        $this->assertCount(1, $gridItems);
+        $this->assertCount(1, $results);
     }
 
     public function test_it_can_return_ids()
     {
-        $gridItems = $this->getMysqlGridRepository()->getOrderIds();
+        $results = $this->orderContext->repos()->orderGridRepository()->getOrderIds();
 
-        $this->assertCount(1, $gridItems);
+        $this->assertCount(1, $results);
     }
 
     public function test_it_can_filter_by_order_reference()
     {
-        $gridItems = $this->getMysqlGridRepository()->filterByOrderReference('xx')->getResults();
+        $results = $this->orderContext->repos()->orderGridRepository()
+            ->filterByOrderReference('order-aaa')
+            ->getResults();
 
-        $this->assertCount(1, $gridItems);
+        $this->assertCount(1, $results);
+
+        $results = $this->orderContext->repos()->orderGridRepository()
+            ->filterByOrderReference('unknown')
+            ->getResults();
+
+        $this->assertCount(0, $results);
     }
 
     public function test_it_can_filter_by_shopper_email()
     {
-        $gridItems = $this->getMysqlGridRepository()->filterByShopperEmail('ben@think')->getResults();
+        $results = $this->orderContext->repos()->orderGridRepository()
+            ->filterByShopperEmail('order-aaa-shopper-aaa@thinktomorrow.be')
+            ->getResults();
 
-        $this->assertCount(1, $gridItems);
+        $this->assertCount(1, $results);
+
+        $results = $this->orderContext->repos()->orderGridRepository()
+            ->filterByShopperEmail('unknown')
+            ->getResults();
+
+        $this->assertCount(0, $results);
     }
 
     public function test_it_can_filter_by_customer_id()
     {
-        $gridItems = $this->getMysqlGridRepository()->filterByCustomerId('ccc-123')->getResults();
-        $this->assertCount(1, $gridItems);
+        // Attach customer id to order
+        $customer = $this->orderContext->createCustomer();
+        $this->orderContext->addCustomerToOrder($this->order, $customer);
 
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByCustomerId('ccc')->getResults());
+        $results = $this->orderContext->repos()->orderGridRepository()
+            ->filterByCustomerId('customer-aaa')
+            ->getResults();
+
+        $this->assertCount(1, $results);
+
+        $results = $this->orderContext->repos()->orderGridRepository()
+            ->filterByCustomerId('unknown')
+            ->getResults();
+
+        $this->assertCount(0, $results);
     }
 
     public function test_it_can_filter_by_order_states()
     {
-        $this->assertCount(1, $this->getMysqlGridRepository()->filterByStates([DefaultOrderState::cart_revived->value])->getResults());
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByStates([DefaultOrderState::cancelled->value])->getResults());
+        $this->assertCount(1, $this->orderContext->repos()->orderGridRepository()->filterByStates([DefaultOrderState::cart_pending->value])->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByStates([DefaultOrderState::cancelled->value])->getResults());
     }
 
     public function test_it_can_filter_by_confirmed_at()
     {
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByConfirmedAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByConfirmedAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
 
         // Force timestamp
-        $this->updateRow('xxx', ['confirmed_at' => now()->toDateTimeString()]);
+        $this->updateRow('order-aaa', ['confirmed_at' => now()->toDateTimeString()]);
 
-        $this->assertCount(1, $this->getMysqlGridRepository()->filterByConfirmedAt(now()->subDay()->toDateTimeString(), now()->addDay()->toDateTimeString())->getResults());
-        $this->assertCount(1, $this->getMysqlGridRepository()->filterByConfirmedAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByConfirmedAt(now()->addDay()->toDateTimeString())->getResults());
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByConfirmedAt(null, now()->subDay()->toDateTimeString())->getResults());
+        $this->assertCount(1, $this->orderContext->repos()->orderGridRepository()->filterByConfirmedAt(now()->subDay()->toDateTimeString(), now()->addDay()->toDateTimeString())->getResults());
+        $this->assertCount(1, $this->orderContext->repos()->orderGridRepository()->filterByConfirmedAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByConfirmedAt(now()->addDay()->toDateTimeString())->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByConfirmedAt(null, now()->subDay()->toDateTimeString())->getResults());
     }
 
     public function test_it_can_filter_by_delivered_at()
     {
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByDeliveredAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByDeliveredAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
 
         // Force timestamp
-        $this->updateRow('xxx', ['delivered_at' => now()->toDateTimeString()]);
+        $this->updateRow('order-aaa', ['delivered_at' => now()->toDateTimeString()]);
 
-        $this->assertCount(1, $this->getMysqlGridRepository()->filterByDeliveredAt(now()->subDay()->toDateTimeString(), now()->addDay()->toDateTimeString())->getResults());
-        $this->assertCount(1, $this->getMysqlGridRepository()->filterByDeliveredAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByDeliveredAt(now()->addDay()->toDateTimeString())->getResults());
-        $this->assertCount(0, $this->getMysqlGridRepository()->filterByDeliveredAt(null, now()->subDay()->toDateTimeString())->getResults());
+        $this->assertCount(1, $this->orderContext->repos()->orderGridRepository()->filterByDeliveredAt(now()->subDay()->toDateTimeString(), now()->addDay()->toDateTimeString())->getResults());
+        $this->assertCount(1, $this->orderContext->repos()->orderGridRepository()->filterByDeliveredAt(now()->subDay()->toDateString(), now()->addDay()->toDateString())->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByDeliveredAt(now()->addDay()->toDateTimeString())->getResults());
+        $this->assertCount(0, $this->orderContext->repos()->orderGridRepository()->filterByDeliveredAt(null, now()->subDay()->toDateTimeString())->getResults());
     }
 
     public function test_it_can_sort_by_confirmed_at()
@@ -117,18 +137,18 @@ class OrderGridRepositoryTest extends TestCase
 
     private function runTestSortingByDate(string $column, string $sortingMethod, string $modelMethod)
     {
-        $order = $this->createOrder(['order_id' => 'yyy', 'order_ref' => 'yy-ref', 'invoice_ref' => 'yy-invoice-ref'], [], [], [], [], null, null, $this->createOrderShopper(['shopper_id' => 'sss']));
-        (new TestContainer())->get(MysqlOrderRepository::class)->save($order);
+        // Add second order with different timestamp to test sorting
+        $order2 = $this->orderContext->createDefaultOrder('order-bbb');
 
-        $this->updateRow('xxx', [$column => now()->addHour()->toDateTimeString()]);
-        $this->updateRow('yyy', [$column => now()->toDateTimeString()]);
+        $this->updateRow('order-aaa', [$column => now()->addHour()->toDateTimeString()]);
+        $this->updateRow('order-bbb', [$column => now()->toDateTimeString()]);
 
-        $gridItems = $this->getMysqlGridRepository()->{$sortingMethod}()->getResults();
+        $results = $this->orderContext->repos()->orderGridRepository()->{$sortingMethod}()->getResults();
 
-        $this->assertCount(2, $gridItems);
+        $this->assertCount(2, $results);
 
         $previousDatetime = null;
-        foreach ($gridItems as $gridItem) {
+        foreach ($results as $gridItem) {
             $dateTime = $gridItem->{$modelMethod}();
 
             if ($previousDatetime) {
@@ -141,16 +161,6 @@ class OrderGridRepositoryTest extends TestCase
 
             $previousDatetime = $dateTime;
         }
-    }
-
-    private function getMysqlGridRepository()
-    {
-        (new TestContainer())->add(OrderGridItem::class, DefaultOrderGridItem::class);
-
-        return new MysqlOrderGridRepository(
-            new TestContainer(),
-            new TestTraderConfig(),
-        );
     }
 
     private function updateRow(string $orderId, array $values)
