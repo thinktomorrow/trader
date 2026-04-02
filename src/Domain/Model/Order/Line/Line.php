d4ac482e19389523f4465d9ff804dcd0a6fe3fba
BenCavens <BenCavens@users.noreply.github.com>
2026-04-02 20:30:36 +0000

    Fix styling

--- src/Domain/Model/Order/Line/Line.php
+++ src/Domain/Model/Order/Line/Line.php
@@ -42,7 +42,9 @@ final class Line implements ChildAggregate, DiscountableItem
 
     private Quantity $quantity;
 
-    private function __construct() {}
+    private function __construct()
+    {
+    }
 
     public static function create(OrderId $orderId, LineId $lineId, PurchasableReference $purchasableReference, ItemPrice $unitPrice, Quantity $quantity, array $data): static
     {
